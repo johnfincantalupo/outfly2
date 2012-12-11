86f0d9d5d5dc85c7aa32785fdf18c8ab6e5da4b7
John F. Incantalupo <john.incantalupo@gmail.com>
2012-12-11 14:43:54 -0500

    Improve the README doc,

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Outfly2::Application.routes.draw do
+
   devise_for :users
 
-  get "static_pages/home"
+  root :to => 'static_pages#home'
 
-  get "static_pages/about"
+  get "about" => "static_pages#about"
 
-  get "static_pages/contact"
+  get "contact" => "static_pages#contact"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +58,7 @@ Outfly2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
