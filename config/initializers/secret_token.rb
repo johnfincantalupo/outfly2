bea0b70c54dc65c4d3f09cf18a3de835c15e77a5
John F. Incantalupo <john.incantalupo@gmail.com>
2012-12-11 14:11:01 -0500

    Init with devise and rspec gems along with StaticPages controller and PostgreSQL database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Outfly2::Application.config.secret_token = '3fc2e65815e80fbaad5700fae0dbea05c4d7c54ad46839c563c0f099b1a4f16c2157d67ca79d51ecf58412bc74c2755ee2c545e0e024ad1fe57d626169535590'
